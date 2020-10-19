93ec9fd32303c384ef99188873762c2fd1955599
jia <sunjaekko>
2020-10-19 22:59:33 +0800

    详情页商品展示

--- src/views/detail/childComps/DetailBaseInfo.vue
+++ src/views/detail/childComps/DetailBaseInfo.vue
@@ -1,7 +1,26 @@
 <template>
-  <div>
-      {{goods.title}}
-      {{goods.newPrice}}
+  <div v-if="Object.keys(goods).length !== 0" class="base-info">
+    <div class="info-title">{{ goods.title }}</div>
+    <div class="info-price">
+      <span class="n-price">{{ goods.newPrice }}</span>
+      <span class="o-price">{{ goods.oldPrice }}</span>
+      <span v-if="goods.discount" class="discount">{{ goods.discount }}</span>
+    </div>
+    <div class="info-other">
+      <span>{{ goods.columns[0] }}</span>
+      <span>{{ goods.columns[1] }}</span>
+      <span>{{ goods.services[goods.services.length - 1].name }}</span>
+    </div>
+    <div class="info-service">
+      <span
+        v-for="index in goods.services.length - 1"
+        :key="index"
+        class="info-service-item"
+      >
+        <img :src="goods.services[index - 1].icon" />
+        <span>{{ goods.services[index - 1].name }}</span>
+      </span>
+    </div>
   </div>
 </template>
 <script>
@@ -15,7 +34,61 @@ export default {
       },
     },
   },
-}
+};
 </script>
 <style scoped>
+.base-info {
+  padding: 0 8px;
+  margin-top: 15px;
+  border-bottom: 5px solid #f2f5f8;
+  color: #999;
+}
+.info-title {
+  color: #222;
+}
+.info-price {
+  margin-top: 10px;
+}
+.info-price .n-price {
+  font-size: 24px;
+  color: var(--color-high-text);
+}
+.info-price .o-price {
+  font-size: 13px;
+  margin-left: 5px;
+  text-decoration: line-through;
+}
+.info-price .discount {
+  font-size: 12px;
+  padding: 2px 5px;
+  color: #fff;
+  background-color: var(--color-high-text);
+  border-radius: 8px;
+  margin-left: 5px;
+  position: relative;
+  top: -7px;
+}
+.info-other {
+  margin-top: 15px;
+  line-height: 15px;
+  font-size: 13px;
+  display: flex;
+  justify-content: space-between;
+  border-bottom: 1px solid rgba(100, 100, 100, 0.1);
+}
+.info-service {
+  display: flex;
+  justify-content: space-between;
+  line-height: 60px;
+}
+.info-service-item img {
+  width: 14px;
+  height: 14px;
+  position: relative;
+  top: 2px;
+}
+.info-service-item span {
+  font-size: 13px;
+  color: #333;
+}
 </style>
